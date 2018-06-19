c468ee9e2906ae6c006e65f88d50ae5ca3e54b0c
sdees82 <seantedees1982@gmail.com>
2018-06-19 17:02:06 -0400

    added google maps

---
+++ src/components/googleMaps/googleMaps.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import './googleMaps.css';
+import GoogleMapReact from 'google-map-react'
+const AnyReactComponent = ({ text }) => <div>{ text }</div>;
+
+
+class GoogleMaps extends Component {
+  static defaultProps = {
+    center: { lat: 40.7446790, lng: -73.9485420 },
+    zoom: 11
+  }
+render() {
+    return (
+      <div className='googleMaps'>
+        <GoogleMapReact
+          defaultCenter={ this.props.center }
+          defaultZoom={ this.props.zoom }>
+          <AnyReactComponent
+            lat={ 40.7473310 }
+            lng={ -73.8517440 }
+            text={ 'Where\'s Waldo?' }
+          />
+        </GoogleMapReact>
+      </div>
+    )
+  }
+}
+
+export default GoogleMaps;
--- src/components/searchDoctor/searchDoctor.js
+++ src/components/searchDoctor/searchDoctor.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './searchDoctor.css';
+import GoogleMaps from "../googleMaps/googleMaps";
 
 
 const SearchDoctor = () =>{
     return (
-        <div className="searchDoctor">
-        
-        </div>
+        <section className="searchDoctor">
+        <GoogleMaps/>
+            <div className="searchCard">
+            <header>
+                <h4>Search for a Doctor</h4>
+                <form>
+                    <select className="specialization">
+                        <option selected>Specialization</option>
+                        <option >Cardiology</option>
+                        <option >Geriatrics</option>
+                        <option >Endocrinology</option>
+                        <option >General practice</option>
+                    </select>
+                    <select className="city">
+                        <option selected>City</option>
+                        <option >Windsor</option>
+                        <option >Leamington</option>
+                        <option >London</option>
+                        <option >Chatham-Kent</option>
+                        <option >Toronto</option>
+                    </select>
+                    </form>
+                </header>
+            </div>
+        </section>
     );
 }
 
