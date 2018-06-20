3b326dad1b8d95c4700766b0b0f55df50e46904b
sdees82 <seantedees1982@gmail.com>
2018-06-20 15:33:44 -0400

    styling changes

--- src/components/payment/payment.js
+++ src/components/payment/payment.js
@@ -2,34 +2,64 @@ import React from 'react';
 import './payment.css';
 import Mastercard from '../../images/mastercard.png';
 
-const Payment =()=>{
-    return(
-        <div className="payment"> 
-        <h1>ADD CARD TO FILE</h1>
-        <div className="container">
-            <header className="cardTop">
-                <img className="mastercard" src={Mastercard} alt="mastercard logo"/>
-            <div className="details">
-                <p>PAYMENT</p>
-                <p>DETAILS</p>
+class Payment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            submitted: "false"
+        }
+    }
+
+    addCard = (e) =>{
+        e.preventDefault();
+        alert("clicked!");
+        this.setState({submitted: "true"});
+        console.log(this.state.submitted);
+    } 
+ 
+    render(){
+        const {submitted} = this.state;
+
+        if(this.state.submitted === " false"){
+            return(
+                <div className="payment"> 
+                <h1>Thank You</h1>
+                <p>Your card has been added</p>
             </div>
-            </header>
-            <div className="cardBottom">
-                <form>
-                    <input className="name" type="text" placeholder="LEBRON JAMES"/>
-                    <div className="cardNumber">
-                        <input className="number" type="text" placeholder="XXXX XXXX XXXX XXXX"/>
-                        <input className="expiration" type="text" placeholder="MONTH | YEAR"/>
+            );
+           
+        }else{
+            return(
+                <div className="payment"> 
+                <h1>ADD CARD TO FILE</h1>
+                <div className="container">
+                    <header className="cardTop">
+                        <img className="mastercard" src={Mastercard} alt="mastercard logo"/>
+                    <div className="details">
+                        <p>PAYMENT</p>
+                        <p>DETAILS</p>
                     </div>
-                    <div className="ccvContainer">
-                    <input className="ccv" type="text" placeholder="343"/>
-                    <button>ENTER</button>
+                    </header>
+                    <div className="cardBottom">
+                        <form>
+                            <input className="name" type="text" placeholder="LEBRON JAMES"/>
+                            <div className="cardNumber">
+                                <input className="number" type="text" placeholder="XXXX XXXX XXXX XXXX"/>
+                                <input className="expiration" type="text" placeholder="MONTH | YEAR"/>
+                            </div>
+                            <div className="ccvContainer">
+                            <input className="ccv" type="text" placeholder="343"/>
+                            <button onClick={this.addCard}>ENTER</button>
+                            </div>
+                        </form>
                     </div>
-                </form>
-            </div>
-            </div>
-        </div>
-    );
+                    </div>
+                </div>
+            );   
+           
+        }
+       
+    }
 }
 
 export default Payment;
